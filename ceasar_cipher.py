9f1c4135626a65a45128faf74a026bb52f0ddd91
Aditya-20hub <aditya.deokar@vaastav.tech>
2026-03-26 15:45:49 +0530

    Added shift function to the file

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -7,3 +7,20 @@ Caesar Cipher Utility Function
 This function is responsible for shifting characters in a given text
 to perform both encryption and decryption using the Caesar cipher technique.
 """
+
+def shift_txt(txt: str, shift: int) -> str:
+    digits = "0123456789"
+    symbols = "!@#$%^&*()_+-=[]{};:,.<>/?"
+    result = ""
+    for char in txt:
+        if char.isupper():
+            result += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        elif char.islower():
+            result += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        elif char in digits:
+            result += digits[(digits.index(char) + shift) % 10]
+        elif char in symbols:
+            result += symbols[(symbols.index(char) + shift) % len(symbols)]
+        else:
+            result += char
+    return result
